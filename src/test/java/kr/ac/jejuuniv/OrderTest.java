3c3cf3202157e5688e3479b8b1430e0c93bba437
Flask <flask@Flaskui-MacBook-Air.local>
2014-08-11 00:20:25 +0900

    add/change some tests

--- src/test/java/kr/ac/jejuuniv/OrderTest.java
+++ src/test/java/kr/ac/jejuuniv/OrderTest.java
@@ -15,6 +15,7 @@ import kr.ac.jejuuniv.config.WebMvcConfig;
 import kr.ac.jejuuniv.domain.model.Item;
 import kr.ac.jejuuniv.domain.model.Order;
 import kr.ac.jejuuniv.domain.model.OrderRepository;
+import kr.ac.jejuuniv.domain.model.QItem;
 import kr.ac.jejuuniv.domain.model.QOrder;
 import static org.hamcrest.core.Is.*;
 import static org.hamcrest.core.IsNot.*;
@@ -44,49 +45,25 @@ public class OrderTest {
 	@Resource
 	private OrderRepository orderRepository;
 
-	QOrder qorder = QOrder.order;
+	private QOrder qorder = QOrder.order;
 
-	@Test
-	public void order() {
-		List<Item> items = new ArrayList<Item>();
-		Item apple = new Item("apple", 100, 3);
-		Item banana = new Item("banana", 20, 10);
-
-		items.add(apple);
-		items.add(banana);
-		Order entity = new Order("flask", items);
-		orderRepository.save(entity);
-
-		List<Order> orders = orderRepository.findAll();
-		assertEquals(1, orders.size());
-		Order order = orders.get(0);
-		assertEquals("flask", order.getCustomer());
-		assertEquals(2, order.getItems().size());
-		assertTrue(order.getItems().contains(apple));
-		assertTrue(order.getItems().contains(banana));
-
-		Item next2 = order.getItems().iterator().next();
-		next2.setPrice(999999);
-		orderRepository.save(order);
-
-		order.getItems().add(new Item("mango", 200, 2));
-		orders = orderRepository.findAll();
-		order = orders.get(0);
-		assertEquals(3, order.getItems().size());
-	}
+	private Item apple = new Item("Apple", 100, 1);
+	private Item banana = new Item("Banana", 150, 2);
+	private Item cucumber = new Item("Cucumber", 400, 5);
 
 	@Test
 	public void addOrderTest() {
 		List<Item> items = new ArrayList<Item>();
-		items.add(new Item("Apple", 100, 1));
-		items.add(new Item("Banana", 150, 2));
-		items.add(new Item("Guava", 400, 5));
+		items.add(apple);
+		items.add(banana);
+		items.add(cucumber);
 		Order order = new Order("flask", items);
 		orderRepository.save(order);
 
 		Order insertedOrder = orderRepository.findOne(order.getId());
 		assertThat(insertedOrder.getCustomer(), is("flask"));
 		assertThat(insertedOrder.getItems().size(), is(3));
+
 		ArrayList<Item> insertedItems = new ArrayList<Item>(insertedOrder.getItems());
 		for (Item item : items)
 			assertThat(insertedItems.contains(item), is(true));
@@ -94,56 +71,119 @@ public class OrderTest {
 
 	@Test
 	public void addAndChangeTest() {
-		List<Item> items = new ArrayList<Item>();
-		Item apple = new Item("Apple", 100, 1);
-		items.add(apple);
-		items.add(new Item("Banana", 150, 2));
-		items.add(new Item("Guava", 400, 5));
-		Order order = new Order("flask", items);
+		Order order = createSampleOrder();
+		Item durian = new Item("Durian", 1000, 2);
+		order.getItems().add(durian);
 		orderRepository.save(order);
 
-		apple.setPrice(200);
-		apple.setQuantity(2);
+		durian.setPrice(1200);
+		durian.setQuantity(3);
 
 		Order insertedOrder = orderRepository.findOne(order.getId());
-		ArrayList<Item> insertedItems = new ArrayList<Item>(insertedOrder.getItems());
-		Item insertedApple = null;
-		for (Item item : insertedItems) {
-			if (item.getProduct().equals("Apple")) {
-				insertedApple = item;
-				break;
-			}
-		}
+		Item insertedItem = findItemByProductName(insertedOrder, "Durian");
 
-		assertThat(insertedApple, is(notNullValue()));
-		assertThat((int) insertedApple.getPrice(), is((int) 200));
-		assertThat((int) insertedApple.getQuantity(), is(2));
+		assertNotNull(insertedItem);
+		assertThat((int) insertedItem.getPrice(), is((int) 1200)); // ? WTF?
+		assertThat((int) insertedItem.getQuantity(), is(3));
 	}
 
 	@Test
-	public void jpaFetchTest() {
-		List<Item> items = new ArrayList<Item>();
-		Item apple = new Item("Apple", 100, 1);
-		items.add(apple);
-		items.add(new Item("Banana", 150, 2));
-		items.add(new Item("Guava", 400, 5));
-		Order order = new Order("flask", items);
+	public void queryDslTest() {
+		Order order = createSampleOrder();
 		orderRepository.save(order);
 
 		JPAQuery jpaQuery = new JPAQuery(entityManager);
-		Order insertedOrder = jpaQuery.from(qorder).where(qorder.items.size().gt(1)).uniqueResult(qorder);
+		Order insertedOrder = jpaQuery
+				.from(qorder)
+				.where(qorder.items.size().gt(1))
+				.uniqueResult(qorder);
 
 		assertNotNull(insertedOrder);
 		assertEquals(3, insertedOrder.getItems().size());
-		Item insertedApple = null;
-		for (Item item : insertedOrder.getItems()) {
-			if (item.getProduct().equals("Apple")) {
-				insertedApple = item;
-				break;
-			}
-		}
+		Item insertedApple = findItemByProductName(insertedOrder, "Apple");
 
-		assertThat(insertedApple, is(notNullValue()));
+		assertNotNull(insertedApple);
 		assertEquals(100, (int) insertedApple.getPrice());
 	}
+
+	@Test
+	public void queryDslTest2() {
+		Order order = createSampleOrder();
+		orderRepository.save(order);
+		List<Item> items = new ArrayList<Item>();
+		items.add(apple);
+		items.add(banana);
+		orderRepository.save(new Order("flask", items));
+		assertThat(orderRepository.findAll().size(), is(2));
+
+		JPAQuery jpaQuery = new JPAQuery(entityManager);
+		List<Order> insertedOrders = jpaQuery
+				.from(qorder)
+				.where(qorder.items.any().product.eq("Cucumber"))
+				.list(qorder);
+		// items 에 Cucumber가 있는 Order 조회
+		
+		assertNotNull(insertedOrders);
+		assertThat(insertedOrders.size(), is(1));
+		assertEquals(3, insertedOrders.get(0).getItems().size());
+		Item insertedCucumber = findItemByProductName(insertedOrders.get(0), "Cucumber");
+
+		assertNotNull(insertedCucumber);
+		assertEquals(400, (int) insertedCucumber.getPrice());
+		
+		jpaQuery = new JPAQuery(entityManager);
+		List<Order> insertedOrders2 = jpaQuery
+				.from(qorder)
+				.where(qorder.items.any().product.ne("Cucumber"))
+				.list(qorder);
+		// items 에 Cucumber가 없는 Order 조회
+		
+		assertNotNull(insertedOrders2);
+		assertThat(insertedOrders2.size(), is(1));
+		assertEquals(2, insertedOrders2.get(0).getItems().size());
+	}
+
+	private Order createSampleOrder() {
+		List<Item> items = new ArrayList<Item>();
+		items.add(apple);
+		items.add(banana);
+		items.add(cucumber);
+		return new Order("flask", items);
+	}
+
+	private Item findItemByProductName(Order order, String product) {
+		for (Item item : order.getItems())
+			if (item.getProduct().equals(product))
+				return item;
+		return null;
+	}
+
+	// @Test
+	// public void order() {
+	// List<Item> items = new ArrayList<Item>();
+	// Item apple = new Item("apple", 100, 3);
+	// Item banana = new Item("banana", 20, 10);
+	//
+	// items.add(apple);
+	// items.add(banana);
+	// Order entity = new Order("flask", items);
+	// orderRepository.save(entity);
+	//
+	// List<Order> orders = orderRepository.findAll();
+	// assertEquals(1, orders.size());
+	// Order order = orders.get(0);
+	// assertEquals("flask", order.getCustomer());
+	// assertEquals(2, order.getItems().size());
+	// assertTrue(order.getItems().contains(apple));
+	// assertTrue(order.getItems().contains(banana));
+	//
+	// Item next2 = order.getItems().iterator().next();
+	// next2.setPrice(999999);
+	// orderRepository.save(order);
+	//
+	// order.getItems().add(new Item("mango", 200, 2));
+	// orders = orderRepository.findAll();
+	// order = orders.get(0);
+	// assertEquals(3, order.getItems().size());
+	// }
 }
